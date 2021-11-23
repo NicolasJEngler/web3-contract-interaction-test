21107e311c98555d71589be1132895b1ddb79023
Nicolás J. Engler <nicojengler@gmail.com>
2021-11-23 09:11:36 -0300

    Add approval method & include inputs to set token ID, token price, address and token URI

--- src/App.js
+++ src/App.js
@@ -30,6 +30,11 @@ function App() {
 
   const [walletAddress, setWalletAddress] = useState(null);
 
+  const [address, setAddress] = useState(null),
+        [tokenId, setTokenId] = useState(null),
+        [tokenUri, setTokenUri] = useState(null),
+        [tokenPrice, setTokenPrice] = useState(0);
+
   // We create a mint function on our front-end 
   // (different from the mint function available in the contract interface)
   // Accepts a minterAddress, a tokenURI (could be an IPFS hash), and a *string* value for the NFT in ether
@@ -43,6 +48,13 @@ function App() {
 
   const setApprovalForAll = (operator, approved) => {
     mintingContract.methods.setApprovalForAll(operator, approved)
+      .send({
+        from: walletAddress
+      });
+  };
+
+  const setApproval = (address, tokenId) => {
+    mintingContract.methods.approve(address, tokenId)
       .send({
         from: walletAddress
       });
@@ -70,11 +82,61 @@ function App() {
       }
 
       {
+        // Input to get addresses to interact with contract methods
+        walletAddress ? 
+          <input 
+            type="text" 
+            placeholder="Address to pass as parameter" 
+            style={{ marginRight: 15 }} 
+            onChange={(e) => {setAddress(e.target.value)}}
+          /> :
+          null
+      }
+
+      {
+        // Input to get addresses to interact with contract methods
+        walletAddress ? 
+          <input 
+            type="text" 
+            placeholder="Token ID to pass as parameter" 
+            style={{ marginRight: 15 }} 
+            onChange={(e) => {setTokenId(e.target.value)}}
+          /> :
+          null
+      }
+
+      {
+        // Input to get token URI to interact with contract methods
+        walletAddress ? 
+          <input 
+            type="text" 
+            placeholder="Token URI to pass as parameter" 
+            style={{ marginRight: 15 }} 
+            onChange={(e) => {setTokenUri(e.target.value)}}
+          /> :
+          null
+      }
+
+      {
+        // Input to set token price to interact with contract methods
+        walletAddress ? 
+          <input 
+            type="number" 
+            defaultValue="0"
+            placeholder="Token price to pass as parameter (ETH)" 
+            onChange={(e) => {setTokenPrice(e.target.value)}}
+          /> :
+          null
+      }
+
+      <br />
+
+      {
         // This call to the front-end mint function passes the current wallet address connected,
         // a hardcoded tokenURI of 3, but should be if possible an IPFS hash
         // and a value in ether as a string
         walletAddress ? 
-          <button onClick={() => {mint(walletAddress, 3, '0.1')}}>Mint!</button> :
+          <button onClick={() => {mint(walletAddress, tokenUri, String(tokenPrice))}}>Mint!</button> :
           null
       }
 
@@ -86,9 +148,22 @@ function App() {
         // in the minting contract.
         walletAddress ? 
           <button onClick={() => {setApprovalForAll(
-            '0xEC17ce447fD60Eab2ec6cB8C1438f607a3E06935', // Marketplace contract address
+            address, // Marketplace contract address
             true
           )}}>Approve tokens for transactions</button> :
+          null
+      }
+
+      <br />
+
+      {
+        // This approves a particular token for transactions done by third-party
+        // smart contracts, i.e.: listing for sale, transferring, etcetera
+        walletAddress ? 
+          <button onClick={() => {setApproval(
+            address, // Marketplace contract address
+            tokenId
+          )}}>Approve single token for transaction</button> :
           null
       }
 
@@ -100,10 +175,10 @@ function App() {
         // as well as end date for the auction and floor price in WEI
         walletAddress ? 
           <button onClick={() => {listEnglishAuction(
-            '0x2Aa0724852CdCbe74B7737075885d538896a1FaF',
-            131,
+            address,
+            tokenId,
             Math.floor(new Date().getTime() / 1000) + 3600, // 3600s = 1h. Auction ends in 1 hour.
-            Web3.utils.toWei('0.5', 'ether')
+            Web3.utils.toWei(String(tokenPrice), 'ether')
           )}}>List as English auction!</button> :
           null
       }
